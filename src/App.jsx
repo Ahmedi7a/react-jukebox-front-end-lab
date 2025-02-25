c8bc9463dbdfff3be209b9377a8af6e3df932830
Ahmedi7a <ahmedamralla73@email.com>
2025-02-25 23:06:52 +0300

    fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,38 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import * as trackService from './services/trackService';
-import NowPlaying from '../components/NowPlaying '
-import TrackList from '../components/TrackList'
-import TrackForm from '../components/TrackForm'
+import NowPlaying from './components/NowPlaying '
+import TrackList from './components/TrackList'
+import TrackForm from './components/TrackForm'
 
 function App() {
-  return(
+
+  const [trackList, setTrackList] = useState([])
+
+  useEffect(() => {
+    async function fetchTracks() {
+      try {
+        const tracks = await trackService.index()
+
+        if (tracks.error) {
+          throw new Error(tracks.error)
+        }
+        setTrackList(tracks) //form the service
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchTracks()
+  }, [])
+
+  return (
     <>
-    <h1>Hello</h1>
+      <h1>Hello</h1>
+      <TrackList trackList={trackList} />
+    
     </>
   )
-  
+
 }
 
 export default App
---
+++ src/services/trackService.js
@@ -0,0 +1,15 @@
+const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
+
+async function index() {
+    try {
+        const res = await fetch(BASE_URL)
+        const tracks = await res.json();
+        return tracks;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export {
+    index,
+}
