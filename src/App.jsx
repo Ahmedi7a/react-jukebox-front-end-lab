f2b06cd2a3316ad2be34b062a5b10fa65ea4f369
Ahmedi7a <ahmedamralla73@email.com>
2025-02-26 22:39:11 +0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,8 @@ import TrackForm from './components/TrackForm'
 function App() {
 
   const [trackList, setTrackList] = useState([])
+  const [selected, setSelected] = useState(null)
+  const [isFormOpen, setIsFormOpen] = useState(false);
 
   useEffect(() => {
     async function fetchTracks() {
@@ -25,11 +27,90 @@ function App() {
     fetchTracks()
   }, [])
 
+  function updateSelected(track) {
+    setSelected(track);
+  }
+
+  function handleFormView(track) {
+    // if (!track.title) setSelected(null);
+    // setIsFormOpen(!isFormOpen);
+    setSelected(track);
+    setIsFormOpen(!isFormOpen);
+  }
+
+  //for creating new Track
+  async function handleAddTrack(formData) {
+    try {
+      const newTrack = await trackService.create(formData);
+
+      if (newTrack.error) {
+        throw new Error(newTrack.error);
+      }
+
+      setTrackList([...trackList, newTrack]);
+      setIsFormOpen(false);
+    } catch (error) {
+      // Log the error to the console
+      console.log(error);
+    }
+  }
+
+  //for updating
+  async function handleUpdateTrack(id, formData) {
+    try {
+      const updatedTrack = await trackService.update(id, formData)
+
+      if (updatedTrack.error) throw new Error(updatedTrack.error)
+
+      const updatedList = trackList.map(track => {
+        // If the id of the current track is not the same as the updated track's id, return the existing track. If the id's match, instead return the updated track.
+        if (track._id === updatedTrack._id) {
+          return updatedTrack
+        }
+        return track
+      })
+
+      setSelected(updatedTrack)
+      setTrackList(updatedList)
+      setIsFormOpen(false)
+
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  async function handleRemoveTrack(id) {
+    try {
+      const response = await trackService.deleteTrack(id);
+
+      if (response.error) {
+        throw new Error(response.error);
+      }
+
+      setTrackList(trackList.filter((track) => track._id !== id));
+      setSelected(null);
+      setIsFormOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <>
-      <h1>Hello</h1>
-      <TrackList trackList={trackList} />
-    
+      <TrackList
+        trackList={trackList}
+        updateSelected={updateSelected}
+        handleUpdateTrack={handleUpdateTrack}
+        handleRemoveTrack={handleRemoveTrack}
+        handleFormView={handleFormView} 
+        isFormOpen={isFormOpen}
+      />
+      {/* if true show the form, if false show details */}
+      {isFormOpen ? (
+        <TrackForm selected={selected} handleAddTrack={handleAddTrack} handleUpdateTrack={handleUpdateTrack} />
+      ) : (
+        <NowPlaying selected={selected} handleFormView={handleFormView} />
+      )}
     </>
   )
 
--- src/services/trackService.js
+++ src/services/trackService.js
@@ -10,6 +10,63 @@ async function index() {
     }
 }
 
+async function create(formData) {
+    try {
+        const res = await fetch(BASE_URL, {
+            // We specify that this is a 'POST' request
+            method: 'POST',
+            // We're sending JSON data, so we attach a Content-Type header
+            // and specify that the data being sent is type 'application/json'
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            // The formData, converted to JSON, is sent as the body
+            // This will be received on the backend as req.body
+            body: JSON.stringify(formData),
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+}
+
+const update = async (id, formData) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(formData),
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+const deleteTrack = async (id) => {
+    try {
+        const res = await fetch(`${BASE_URL}/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        });
+        return res.json();
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+
+
+
+//======================================
 export {
     index,
+    create,
+    update,
+    deleteTrack,
 }
